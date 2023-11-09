b2272b2b44fc70e4298fc299a6c02b9cd3a21e87
Madeline Urbanic <madelinerurbanic@gmail.com>
2023-11-08 21:05:08 -0500

    add get/post

--- server.js
+++ server.js
@@ -6,11 +6,30 @@ const app = express();
 
 app.use(express.static('public'));
 
-
-
-
-
-
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+  });
+  
+  
+  // GET request
+  app.get('/api/notes', (req, res) => {
+      // Let the client know that their request was received
+      res.json(`${req.method} request received`);
+    
+      // Set the notes to the client
+      return res.json(notesData);
+    });
+    
+    // POST request
+    app.post('/api/notes', (req, res) => {
+      // Let the client know that their POST request was received
+      res.json(`${req.method} request received`);
+    
+      // add error handling HERE
+  
+      // log response HERE
+  
+    });
   
   app.listen(PORT, () =>
     console.log(`Express server listening on port ${PORT}!`)
