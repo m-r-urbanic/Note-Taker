734a46d04cab92c0586f05a57c112c2dbdc676fe
Madeline Urbanic <madelinerurbanic@gmail.com>
2023-11-08 21:42:46 -0500

    change port to local for testing

---
+++ README.md
@@ -0,0 +1,16 @@
+# Notes-App
+
+## Description
+This application allows users to take notes and save the notes.
+
+## Installation
+There is no installation needed, open up the webpage to access the application.
+
+## Usage
+
+
+## Credits
+All code/resources used are written by myself or provided by the bootcamp.
+
+## License
+MIT License
--- server.js
+++ server.js
@@ -1,37 +1,44 @@
 const express = require('express');
-const notesData = require('./db/db.json');
-const PORT = 3001;
+const note = require('./db/db.json');
+const PORT = process.env.port || 3001;
 
 const app = express();
 
 app.use(express.static('public'));
 
+// GET Route for initial page
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+  res.sendFile(path.join(__dirname, '/public/index.html'));
+});
+
+// GET Route for notes
+app.get('/feedback', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+
+// GET request
+app.get('/api/notes', (req, res) => {
+    // Let the client know that their request was received
+    res.json(`${req.method} request received`);
+  
+    // Set the notes to the client
+    return res.json(note);
   });
   
+  // POST request
+  app.post('/api/notes', (req, res) => {
+    // Let the client know that their POST request was received
+    res.json(`${req.method} request received`);
   
-  // GET request
-  app.get('/api/notes', (req, res) => {
-      // Let the client know that their request was received
-      res.json(`${req.method} request received`);
-    
-      // Set the notes to the client
-      return res.json(notesData);
-    });
-    
-    // POST request
-    app.post('/api/notes', (req, res) => {
-      // Let the client know that their POST request was received
-      res.json(`${req.method} request received`);
-    
-      // add error handling HERE
+    // add error handling HERE
+
+    // log response HERE
+
+  });
   
-      // log response HERE
-  
-    });
-  
-  app.listen(PORT, () =>
-    console.log(`Express server listening on port ${PORT}!`)
-  );
+// change port to local for testing
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT}`)
+);
   
